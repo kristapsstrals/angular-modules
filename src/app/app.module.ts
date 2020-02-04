5cdb1cbdc9b778ac48120f4f00a6ec43bf273fc1
Kristaps Dzērve-Štrāls <kristaps@kriss.tech>
2020-02-04 16:23:32 +0200

    navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LandingComponent } from './pages/landing/landing.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LandingComponent } from "./pages/landing/landing.component";
+import { NotFoundComponent } from "./pages/not-found/not-found.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
